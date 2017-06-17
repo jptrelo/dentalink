11e872234a50b2584cdc707cf4d86d7ebbe46764
Jhon Trejos <jhontrl0.1@gmail.com>
2017-06-17 01:23:32 -0300

    funcion que asigna el tamaño a cada evento

--- js/app.js
+++ js/app.js
@@ -25,21 +25,19 @@
 
 			// Fetch data
 			$.getJSON("js/data.json",function(r){
-				//var events = r.data;
+
 				r.data.forEach(function(event){
 					//Get each event details
 					var startDate = moment(event.start_at);
 					var endDate = moment(event.due_at); 
-					var positionTop = Schedule.getEventTop(startDate);
-					var eventHeight = Schedule.getEventHeight(startDate, endDate);
 
 					//Build an object 'detailedEvent', which has each event detailed information
 					var detailedEvent = {
 						startDate : startDate,
 						endDate : endDate,
 						day : startDate.day() - 1,
-						top : positionTop,
-						height : eventHeight
+						top : Schedule.getEventTop(startDate),
+						height : Schedule.getEventHeight(startDate, endDate)
 						};
 					Schedule.addEvent(detailedEvent);
 
@@ -56,9 +54,9 @@
 		 * @param {event [Has each event detailed information]
 		 */
 		addEvent: function(event){
-			//Create HTML for an event
+			//Create element for an event
 			var eventHtml = $("<div/>").css({
-				//position : "absolute",
+				position : "absolute",
 				//left : 0,
 				top : event.top + "px",
 				height : event.height + "px"
@@ -67,8 +65,6 @@
 			var column = $(".schedule-container .column-day:eq("+event.day+")");
 			//Append the element created
 			column.append(eventHtml);
-
-			//Schedule.setEventsWidth(column, event);
 
 		},
 		/**
@@ -95,25 +91,53 @@
 
 			return eventHeight;
 		},
+		/**
+		 * [setEventsWidth Sets every width of each events]
+		 */
 		setEventsWidth: function(){
-
+			// Loop each day
 			$($(".schedule-container").children()).each(function(){
 				
 				var column = $(this);
 				var columnEvents = column.children().length;
 
-					$(column.children()).each(function(){
+				// Loop each event to compare if it's in between anothers in the same day and position
+				$(column.children()).each(function(index){
 
-						console.log(this);
+					var eventTop = $(this).position().top;
+					var eventIndex = index;
+					var inBetween = [];
+
+					// Loop each event to compare with the above(key event)
+					$(column.children()).each(function(index){
+
+						// Compare everything but none the 'key event'
+						if (index != eventIndex) {
+							// If it's between any
+							var eventBottom = $(this).position().top + $(this).height();
+							if ((eventTop >= $(this).position().top) && (eventTop < eventBottom)) {
+								//Save with which ones is related
+								inBetween.push(index);
+							} 
+						}
+
 					});
+					// How many are related. 
+					// 0 means no related events(just the 'key event' = 1).
+					// More than 0 means there are related events plus the 'key event'. 
+					var quantity = (inBetween.length > 0) ? inBetween.length + 1 : 1;
 
-				column.children().removeClass();
-				if (columnEvents <= 3) {
-				
-					column.children().addClass('col-xs-' + 12/columnEvents);
-				} else {
-					column.children().addClass('col-xs-4');
-				}
+					// Add a Bootstraps class to set the width up
+					var widthClass = 'col-xs-' + 12/quantity;
+					$(column.children()[index]).addClass(widthClass);
+
+					// Loop if there are related events so we remove the aboves an add a new one
+					inBetween.forEach(function(child){
+						$(column.children()[child]).removeClass();
+						$(column.children()[child]).addClass(widthClass);
+
+					});
+				});
 
 			});
 		}
