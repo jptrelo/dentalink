81d8d7b35efa7feb32be42d7a0d764b25c8297f9
Jhon Trejos <jhontrl0.1@gmail.com>
2017-06-17 12:49:40 -0300

    Ajuste de comentarios

--- js/app.js
+++ js/app.js
@@ -59,7 +59,7 @@
 		},
 		/**
 		 * [addEvent This function append an event to the schedule]
-		 * @param {event [Has each event detailed information]
+		 * @param event [Has each event detailed information]
 		 */
 		addEvent: function(event){
 			//Create element for an event
@@ -75,8 +75,8 @@
 		},
 		/**
 		 * [getEventTop Calculates the top position where an event will be added]
-		 * @param  {startDate [The start date of an event]
-		 * @return {[The top position]
+		 * @param  startDate [The start date of an event]
+		 * @return [The top position]
 		 */
 		getEventTop: function(startDate){
 			//Find and get the position of a div that has same time in attr 'data-hour' 
@@ -86,9 +86,9 @@
 		},
 		/**
 		 * [getEventHeight Calculates the height for an event]
-		 * @param  {startDate [The start date of an event]
-		 * @param  {endDate   [The end date of an event]
-		 * @return {[The event height]
+		 * @param  startDate [The start date of an event]
+		 * @param  endDate   [The end date of an event]
+		 * @return [The event height]
 		 */
 		getEventHeight: function(startDate, endDate){
 			//Find and get the top positions of a divs that has same time in attr 'data-hour'
@@ -127,7 +127,7 @@
 		},
 		/**
 		 * [setEventsLeft Sets left of each event]
-		 * @param {eventsDay [A day to set each event up]
+		 * @param eventsDay [A day to set each event up]
 		 */
 		setEventsLeft: function(eventsDay){
 
@@ -162,10 +162,10 @@
 		},
 		/**
 		 * [getInBetweenRelated Finds the related events that a 'key event' is in between.]
-		 * @param  {eventsDay  [Same day as 'key event' to compare with others events]
-		 * @param  {eventTop   [Top position of a 'key event']
-		 * @param  {eventIndex [A 'key event' index]
-		 * @return {[Return the related events that a 'key event' is in between.]
+		 * @param  eventsDay  [Same day as 'key event' to compare with others events]
+		 * @param  eventTop   [Top position of a 'key event']
+		 * @param  eventIndex [A 'key event' index]
+		 * @return [Return the related events that a 'key event' is in between.]
 		 */
 		getInBetweenRelated: function(eventsDay, eventTop, eventIndex){
 			var inBetween = [];
@@ -187,9 +187,9 @@
 		},
 		/**
 		 * [isInBetween Find if the top position of a 'key event' is between compareEvent]
-		 * @param  {keyEventTop  [The top position of a 'key event']
-		 * @param  {compareEvent [The event to compare positions]
-		 * @return {[True: 'key event' is between compareEvent. False: 'key event' is not between compareEvent]
+		 * @param  keyEventTop  [The top position of a 'key event']
+		 * @param  compareEvent [The event to compare positions]
+		 * @return [True: 'key event' is between compareEvent. False: 'key event' is not between compareEvent]
 		 */
 		isInBetween: function(keyEventTop, compareEvent){
 			// If keyEventTop is between compareEvent
