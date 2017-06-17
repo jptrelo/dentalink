a84b0af903d2fa0646452a794c78aa736879cda0
Jhon Trejos <jhontrl0.1@gmail.com>
2017-06-17 12:45:11 -0300

    Refactoring de funcion que trae los eventos relacionados getInBetweenRelated. Funcionalidad que ajusta la posicion left de los eventos setEventsLeft.

--- js/app.js
+++ js/app.js
@@ -43,7 +43,15 @@
 
 				});
 
-				Schedule.setEventsWidth();
+				// Loop each day to config each element added
+				$($(".schedule-container").children()).each(function(){
+				
+					var eventsDay = $(this);
+
+					Schedule.setEventsWidth(eventsDay);
+					Schedule.setEventsLeft(eventsDay);
+
+				});
 
 			}).error(function(err){
 				console.log(err);
@@ -56,8 +64,6 @@
 		addEvent: function(event){
 			//Create element for an event
 			var eventHtml = $("<div/>").css({
-				position : "absolute",
-				//left : 0,
 				top : event.top + "px",
 				height : event.height + "px"
 			});
@@ -92,54 +98,107 @@
 			return eventHeight;
 		},
 		/**
-		 * [setEventsWidth Sets every width of each events]
+		 * [setEventsWidth Sets width of each event]
+		 * @param {eventsDay [A day to set each event up]
 		 */
-		setEventsWidth: function(){
-			// Loop each day
-			$($(".schedule-container").children()).each(function(){
+		setEventsWidth: function(eventsDay){
+			// Loop each event to compare if it's in between anothers in the same day and position
+			$(eventsDay.children()).each(function(index){
+
+				var eventTop = $(this).position().top;
+				var eventIndex = index;
+				var inBetween = Schedule.getInBetweenRelated(eventsDay, eventTop, eventIndex);
 				
-				var column = $(this);
-				var columnEvents = column.children().length;
+				// How many are related. 
+				// 0 means no related events(just the 'key event' = 1).
+				// More than 0 means there are related events plus the 'key event'. 
+				var relatedEvents = (inBetween.length > 0) ? inBetween.length + 1 : 1;
 
-				// Loop each event to compare if it's in between anothers in the same day and position
-				$(column.children()).each(function(index){
+				// Divide a 100% width between related events
+				var eventWidth = Math.floor(100 / relatedEvents)
+				$(eventsDay.children()[eventIndex]).css({width : eventWidth + "%"});
 
-					var eventTop = $(this).position().top;
-					var eventIndex = index;
-					var inBetween = [];
+				// Loop if there are related events so we remove the aboves an add a new one
+				inBetween.forEach(function(related){
+					$(eventsDay.children()[related]).css({width : eventWidth + "%"});
 
-					// Loop each event to compare with the above(key event)
-					$(column.children()).each(function(index){
+				});
+			});			
+		},
+		/**
+		 * [setEventsLeft Sets left of each event]
+		 * @param {eventsDay [A day to set each event up]
+		 */
+		setEventsLeft: function(eventsDay){
 
-						// Compare everything but none the 'key event'
-						if (index != eventIndex) {
-							// If it's between any
-							var eventBottom = $(this).position().top + $(this).height();
-							if ((eventTop >= $(this).position().top) && (eventTop < eventBottom)) {
-								//Save with which ones is related
-								inBetween.push(index);
-							} 
-						}
+			// Loop each event to compare if it's in between anothers in the same day and position
+			$(eventsDay.children()).each(function(index){
+				var keyEvent = $(this);
+				var keyEventIndex = index;
+				if(keyEventIndex === 0) return keyEvent.css({left : 0});
 
-					});
-					// How many are related. 
-					// 0 means no related events(just the 'key event' = 1).
-					// More than 0 means there are related events plus the 'key event'. 
-					var quantity = (inBetween.length > 0) ? inBetween.length + 1 : 1;
+				var lastEvent;
+				// Loop each event to compare with the above(key event)
+				$(eventsDay.children()).each(function(index){
+					if(keyEventIndex == index) return false;
 
-					// Add a Bootstraps class to set the width up
-					var widthClass = 'col-xs-' + 12/quantity;
-					$(column.children()[index]).addClass(widthClass);
+					// If it's between any
+					if (Schedule.isInBetween(keyEvent.position().top, this)) {
+						//Save with which ones it is related
+						lastEvent = $(this);
+						
+					} else{
+						if(lastEvent) return false;
+					}
 
-					// Loop if there are related events so we remove the aboves an add a new one
-					inBetween.forEach(function(child){
-						$(column.children()[child]).removeClass();
-						$(column.children()[child]).addClass(widthClass);
-
-					});
 				});
 
+				if(lastEvent){
+					var eventLeft = lastEvent.position().left + lastEvent.outerWidth();
+					keyEvent.css({left : eventLeft});
+				}
+
+			});			
+		},
+		/**
+		 * [getInBetweenRelated Finds the related events that a 'key event' is in between.]
+		 * @param  {eventsDay  [Same day as 'key event' to compare with others events]
+		 * @param  {eventTop   [Top position of a 'key event']
+		 * @param  {eventIndex [A 'key event' index]
+		 * @return {[Return the related events that a 'key event' is in between.]
+		 */
+		getInBetweenRelated: function(eventsDay, eventTop, eventIndex){
+			var inBetween = [];
+			// Loop each event to compare with the above(key event)
+			$(eventsDay.children()).each(function(index){
+
+				// Compare everything but none the 'key event'
+				if (index != eventIndex) {
+					// If it's between any
+					if (Schedule.isInBetween(eventTop, this)) {
+						//Save with which ones it is related
+						inBetween.push(index);
+					} 
+				}
+
 			});
+
+			return inBetween;
+		},
+		/**
+		 * [isInBetween Find if the top position of a 'key event' is between compareEvent]
+		 * @param  {keyEventTop  [The top position of a 'key event']
+		 * @param  {compareEvent [The event to compare positions]
+		 * @return {[True: 'key event' is between compareEvent. False: 'key event' is not between compareEvent]
+		 */
+		isInBetween: function(keyEventTop, compareEvent){
+			// If keyEventTop is between compareEvent
+			var eventBottom = $(compareEvent).position().top + $(compareEvent).height();
+			if ((keyEventTop >= $(compareEvent).position().top) && (keyEventTop < eventBottom)) {
+				return true;
+			} else{
+				return false;
+			}
 		}
 	}
 
